933f631a51dd9cc2c3fe08001baa81fd17c4c4fe
Vatsala Shrivastava <vatsalashrivastava12@gmail.com>
2017-02-27 22:26:16 +0530

    Update login.php

--- login.php
+++ login.php
@@ -44,7 +44,7 @@ if(isset($_POST['submitted']))
 
                 <li class="active"><a href="home.html">Home</a></li>
                 <li><a href="About.html">About Us</a></li>
-                <li><a href="contact.html">News</a></li>
+                <li><a href="contact.html">New Trend</a></li>
                 <li class="dropdown">
                     <a class="dropdown-toggle" data-toggle="dropdown" href="#">Jobs
                         <span class="caret"></span></a>
@@ -54,7 +54,7 @@ if(isset($_POST['submitted']))
                         <li><a href="#">Chemical-Engg</a></li>
                     </ul>
                 </li>
-                <li><a href="news.html">Contact</a></li>
+                <li><a href="contact.html">Contact</a></li>
 
             </ul>
 
